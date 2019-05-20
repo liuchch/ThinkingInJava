4638193d765f610cd853f87155d4fa0df7a24639
andy <andy66@lonlife.cn>
2019-05-20 23:59:05 +0800

    section8_1

---
+++ src/section8/InstantiateGenericType.java
@@ -0,0 +1,28 @@
+package section8;
+
+import static util.Print.print;
+
+class ClassAsFactory<T> {
+    T x;
+    public ClassAsFactory(Class<T> kind) {
+        try {
+            x = kind.newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+class Employee{}
+
+public class InstantiateGenericType {
+    public static void main(String[] args) {
+        ClassAsFactory<Employee> fe = new ClassAsFactory<>(Employee.class);
+        print("ClassAsFactory<Employee> succeeded");
+        try {
+            ClassAsFactory<Integer> fi = new ClassAsFactory<>(Integer.class);
+         } catch (Exception e) {
+            print("ClassAsFacroty<Integer> failed" + e.toString());
+        }
+    }
+}
