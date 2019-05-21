603f3338b2a38035c5974ca3350f0586066f5a83
liuliu <842130398@qq.com>
2019-05-21 09:18:11 +0800

    15.8

---
+++ src/chapter15/section8/Erased.java
@@ -0,0 +1,12 @@
+package chapter15.section8;
+
+public class Erased<T> {
+    private final int SIZE = 100;
+    public static void f(Object arg) {
+//        if (arg instanceof T){}
+//        T var = new T();
+//        T[] array = new T[SIZE];
+//        T[] array = (T) new Object[SIZE];
+    }
+
+}
