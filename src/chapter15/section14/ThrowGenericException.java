3ed89d79cbfca91b260d26b36adf13d52db7127d
liuliu <842130398@qq.com>
2019-06-12 22:42:31 +0800

    section14

--- src/chapter15/section14/ThrowGenericException.java
+++ src/chapter15/section14/ThrowGenericException.java
@@ -1,5 +1,7 @@
 package chapter15.section14;
 
+import com.sun.net.httpserver.Authenticator;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,5 +19,58 @@ class ProcessRunner<T, E extends Exception> extends ArrayList<Processor<T, E>> {
     }
 }
 
+class Failure1 extends Exception {}
+
+class Proccessor1 implements Processor<String, Failure1> {
+    static int count = 3;
+    public void process(List<String> resultCollector) throws Failure1 {
+        if (count-- > 1)
+            resultCollector.add("Hep!");
+        else
+            resultCollector.add("Ho!");
+        if (count < 0) {
+            throw new Failure1();
+        }
+    }
+}
+
+class Failure2 extends Exception {}
+
+class Proccessor2 implements Processor<Integer, Failure2> {
+    static int count = 2;
+    public void process(List<Integer> resultCollector) throws Failure2 {
+        if (count-- == 0) {
+            resultCollector.add(47);
+        } else {
+            resultCollector.add(11);
+        }
+        if (count < 0) {
+            throw new Failure2();
+        }
+    }
+}
+
 public class ThrowGenericException {
+    public static void main(String[] args) {
+        ProcessRunner<String, Failure1> runner =
+                new ProcessRunner<>();
+        for (int i=0; i<3; i++) {
+            runner.add(new Proccessor1());
+        }
+        try {
+            System.out.println(runner.processAll());
+        } catch (Failure1 e) {
+            System.out.println(e);
+        }
+
+        ProcessRunner<Integer, Failure2> runner2 = new ProcessRunner<>();
+        for (int i = 0; i < 3; i++) {
+            runner2.add(new Proccessor2());
+        }
+        try {
+            System.out.println(runner2.processAll());
+        } catch (Failure2 e) {
+            System.out.println(e);
+        }
+    }
 }
